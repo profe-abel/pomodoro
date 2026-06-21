4fd7e184182d373a8f8898d8a4c05ce3bb2f756d
profe-abel <cimientodigitalacad@gmail.com>
2026-06-21 15:11:23 -0300

    Add files via upload
    Iconos y PWA

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'focusflow-v1';
+const ASSETS = [
+  '/pomodoro/',
+  '/pomodoro/index.html',
+  '/pomodoro/manifest.json',
+  '/pomodoro/icons/icon-192x192.png',
+  '/pomodoro/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
